997020b0c426e9209a6a1892cd5a1e7c557c8280
Sahil1728 <77491529+Sahil1728@users.noreply.github.com>
2023-01-23 19:41:58 +0000

    Created the main.py file and wrote the basic code

---
+++ src/main.py
@@ -0,0 +1,20 @@
+# importing the required moudules
+import pygame
+import sys
+# importing the const.py file
+from const import *
+
+
+# MAIN CLASS
+class Main:
+    def __init__(self):
+        print("Hi! Hope this works.")
+    # Method for calling all classes (AKA The GAME RUNNER)
+    def mainloop(self):
+        pass
+
+
+# instance of main class
+main = Main()
+# call mainloop method
+main.mainloop()
