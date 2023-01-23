90846db2f4cb239bcc9b91cd153e8f86928e7d71
Sahil1728 <77491529+Sahil1728@users.noreply.github.com>
2023-01-23 19:51:33 +0000

    Edited init function of main for pygame instance

--- src/main.py
+++ src/main.py
@@ -8,6 +8,12 @@ from const import *
 # MAIN CLASS
 class Main:
     def __init__(self):
+        # Initialising the pygame screen
+        pygame.init()
+        # Screen attributes
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        # Title of the screen
+        pygame.display.set_caption("Chess")
         print("Hi! Hope this works.")
     # Method for calling all classes (AKA The GAME RUNNER)
     def mainloop(self):
