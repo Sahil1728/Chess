0345325b731532e4c530c5fda9072a19293be975
Sahil1728 <77491529+Sahil1728@users.noreply.github.com>
2023-01-23 20:05:14 +0000

    game.py for game rendering

---
+++ src/game.py
@@ -0,0 +1,16 @@
+import pygame
+from const import *
+
+
+
+# Responsible for all the rendering methods for now
+
+class Game:
+
+    def __init__(self):
+        pass
+
+    # SHOW METHODS
+
+    def show_background(self, screen):
+        screen.fill((0, 0, 0))
